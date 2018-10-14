f7fb06a983368a27f185cb1243d932cc7d494919
sologgfun <857949825@qq.com>
2018-10-14 23:12:00 +0800

    demo

--- js/background.js
+++ js/background.js
@@ -1,6 +1,9 @@
 var count = 0;
 var winWidth = 400;
 var winHeight = 200;
+var notificationId ;
+var countdownId = 0;
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
 	console.log('收到来自content-script的消息：');
@@ -14,32 +17,59 @@ function timer() {
     if(count <3600){
         count++;
     }
-    console.log(count);
+    if(count == 3600){
+        //通知
+        notificationAction();
+    }
+    // console.log(count);
     return count/36
 }
 
 function refresh(){
-    console.log("here");
+    count = 0;
+    //清除五分钟倒计时
+    window.clearInterval(countdownId);
+
+    //test
+    notificationAction();
+}
+
+var fivemin = 0;
+var Countdown = function(){
+    fivemin ++;
+    if(fivemin==300){
+        notificationAction();
+        fivemin = 0;
+    }
+};
+
+function notificationAction(){
     chrome.notifications.clear("1",
     (id)=>{
-   
-      console.log(id);
-   
+
+    console.log(id);
+
     }    );
     chrome.notifications.create("1", {
         type: 'basic',
         iconUrl: 'img/logo.png',
-        title: '这是标题2',
-        buttons:[{title:"烤鸭",iconUrl: 'img/logo.png'},{title:"烤鸭",iconUrl: 'img/logo.png'}],
-        message: '您刚才点击了自定义右键菜单！'
-    },
-    (id)=>{
-   
-      console.log(id);
-   
-    }    );
-    count = 0;
+        title: '快喝水！',
+        buttons:[{title:"喝水",iconUrl: 'img/yellowsmile.png'},{title:"等会儿",iconUrl: 'img/redsmile.png'}],
+        message: '已经一个小时没喝水了！小鸭子渴死了！'
+    });
 }
+
+//按钮事件触发
+chrome.notifications.onButtonClicked.addListener(function(x, y) {
+    if(y == 0){
+        count = 0;
+        //清除五分钟倒计时
+        window.clearInterval(countdownId);
+    }else{
+        //如果不喝水，就开始五分钟倒计时
+        var countdownId = setInterval(Countdown, 1000);
+    }
+});
 
 function getWandH(){
     var WandH = {winWidth:winWidth,winHeight:winHeight}
